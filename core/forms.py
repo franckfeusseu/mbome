5580010d4e0b641dfe2329bb6423cabdd8adec74
franck fesse <feusseufranck@gmail.com>
2019-12-22 00:40:00 +0000

    second

--- core/forms.py
+++ core/forms.py
@@ -1,14 +1,26 @@
 import re
 
+
+from django import forms
 from django.forms import ModelForm
-from django.core.exceptions import ValidationError
+
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Row, Column
 
 from .models import Adresse
+from .validators import validate_age, validate_telephone
+
 
 class AddressForm(ModelForm):
+    check_me_out = forms.BooleanField(
+        label=('I agree to the terms and conditions and privacy policy'),
+        required=True,
+        error_messages={'required': 'please tick the box'}
+    )
+    dob = forms.DateField(required=True, validators=[validate_age])
+    tel = forms.CharField(required=True, validators=[validate_telephone])
+
     def __init__(self, *args, **kwargs):
         super(AddressForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
@@ -35,6 +47,7 @@ class AddressForm(ModelForm):
                 css_class='form-row'
             ),
             'tel',
+            'check_me_out',
             Submit('submit', 'Submit'))
 
     class Meta:
